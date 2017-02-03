c7a1a6ed9e53a86522bc66ee2b252ee6c71c516e
Leszek Pietrzak <smutnyleszek@gmail.com>
2017-02-03 15:37:16 +0100

    bulding images first raw working version

--- _buildImages.js
+++ _buildImages.js
@@ -28,6 +28,25 @@ const createDir = (dir) => {
 };
 
 // returns all files found in directory (subdirectories included)
+const walkDirFiles = (dir, filelist) => {
+    if (dir[dir.length - 1] !== '/') {
+        dir = dir.concat('/');
+    }
+
+    const files = fs.readdirSync(dir);
+    filelist = filelist || [];
+
+    files.forEach((file) => {
+        if (fs.statSync(dir + file).isDirectory()) {
+            filelist = walkDirFiles(dir + file + '/', filelist);
+        } else {
+            filelist.push(dir + file);
+        }
+    });
+    return filelist;
+};
+
+// returns all files found in directory (subdirectories included)
 const walkDir = (dir, filelist) => {
     if (dir[dir.length - 1] !== '/') {
         dir = dir.concat('/');
@@ -38,9 +57,8 @@ const walkDir = (dir, filelist) => {
 
     files.forEach((file) => {
         if (fs.statSync(dir + file).isDirectory()) {
-            filelist = walkDir(dir + file + '/', filelist);
-        } else {
             filelist.push(dir + file);
+            filelist = walkDir(dir + file + '/', filelist);
         }
     });
     return filelist;
@@ -50,22 +68,31 @@ const walkDir = (dir, filelist) => {
 generating images
 *******************************************************************************/
 
-const allImages = walkDir('src/images');
+const allDirs = walkDir('./src/images');
+console.log('All found dirs:');
+allDirs.forEach((directory) => {
+    console.log(directory);
+});
+
+const allImages = walkDirFiles('./src/images');
 console.log('All found images:');
 allImages.forEach((image) => {
     console.log(image);
 });
 
-allImages.forEach((image) => {
-    console.log(image);
+allDirs.forEach((directory) => {
+    const finalPath = directory.replace('src/', 'dist/');
+    createDir(finalPath);
 });
 
-// gm('src/images/test/medieval-blood-and-gore.jpg')
-// .resize(240, 240)
-// .write('dist/images/test/medieval-blood-and-gore-small.jpg', (err) => {
-//     if (err) {
-//         console.error(err);
-//     } else {
-//         console.log('done');
-//     }
-// });
+allImages.forEach((image) => {
+    console.log(image);
+    const finalPath = image.replace('src/', 'dist/');
+    gm(image).resize(240, 240).write(finalPath, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('done:', image);
+        }
+    });
+});
