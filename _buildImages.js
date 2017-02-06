45c8543ca7258e4af519e6b94381ae1cde9be4ed
Leszek Pietrzak <smutnyleszek@gmail.com>
2017-02-06 11:06:00 +0100

    almost there

--- _buildImages.js
+++ _buildImages.js
@@ -1,11 +1,29 @@
 /*******************************************************************************
 node script for building a nice responsive images from the source directory
+
+NOTE: it can and will overwrite existing files in destinationDirectory
 *******************************************************************************/
 
 const fs = require('fs');
 const gm = require('gm');
 const process = require('process');
 const imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+const sourceDirectory = './src/images';
+const destinationDirectory = './dist/images';
+const imageSizes = {
+    small: {
+        width: 400,
+        height: 400,
+        quality: 80,
+        suffix: '_small'
+    },
+    small_2x: {
+        width: 800,
+        height: 800,
+        quality: 80,
+        suffix: '_small_2x'
+    }
+};
 
 /*******************************************************************************
 helper functions
@@ -29,7 +47,9 @@ const createDir = (dir) => {
 };
 
 // returns all files found in directory (subdirectories included)
-const walkDirFiles = (dir, filelist) => {
+// @param {string} dir - path from which to get the files
+// @param {array} filelist - an optional parameter for the recursive magic
+const getFilesFromDir = (dir, filelist) => {
     if (dir[dir.length - 1] !== '/') {
         dir = dir.concat('/');
     }
@@ -39,7 +59,7 @@ const walkDirFiles = (dir, filelist) => {
 
     files.forEach((file) => {
         if (fs.statSync(dir + file).isDirectory()) {
-            filelist = walkDirFiles(dir + file + '/', filelist);
+            filelist = getFilesFromDir(dir + file + '/', filelist);
         } else {
             filelist.push(dir + file);
         }
@@ -47,66 +67,88 @@ const walkDirFiles = (dir, filelist) => {
     return filelist;
 };
 
-// returns all files found in directory (subdirectories included)
-const walkDir = (dir, filelist) => {
+// returns all directories found in directory (subdirectories included)
+// @param {string} dir - path from which to get the directories
+// @param {array} dirlist - an optional parameter for the recursive magic
+const getDirsFromDir = (dir, dirlist) => {
     if (dir[dir.length - 1] !== '/') {
         dir = dir.concat('/');
     }
 
     const files = fs.readdirSync(dir);
-    filelist = filelist || [];
+    dirlist = dirlist || [];
 
     files.forEach((file) => {
         if (fs.statSync(dir + file).isDirectory()) {
-            filelist.push(dir + file);
-            filelist = walkDir(dir + file + '/', filelist);
+            dirlist.push(dir + file);
+            dirlist = getDirsFromDir(dir + file + '/', dirlist);
         }
     });
-    return filelist;
+    return dirlist;
 };
 
 /*******************************************************************************
 generating images
 *******************************************************************************/
 
-const allDirs = walkDir('./src/images');
-console.log('All found dirs:');
-allDirs.forEach((directory) => {
-    console.log(directory);
-});
+const suffixize = (file, string) => {
+    const dotIndex = file.lastIndexOf('.');
+    if (dotIndex === -1) {
+        return file + string;
+    } else {
+        return file.substring(0, dotIndex) + string + file.substring(dotIndex);
+    }
+};
 
-const allImages = walkDirFiles('./src/images');
-console.log('All found images:');
-allImages.forEach((image) => {
-    console.log(image);
-});
+const convertImage = (fromDir, toDir, imagePath, properties) => {
+    let finalPath = imagePath.replace(fromDir, toDir);
+    finalPath = suffixize(finalPath, properties.suffix);
 
-allDirs.forEach((directory) => {
-    const finalPath = directory.replace('src/', 'dist/');
-    createDir(finalPath);
-});
-
-allImages.forEach((image) => {
-    console.log(image);
-    const finalPath = image.replace('src/', 'dist/');
-    gm(image).resize(240, 240).write(finalPath, (err) => {
+    gm(imagePath)
+    .resize(properties.width, properties.height)
+    .quality(properties.quality)
+    .write(finalPath, (err) => {
         if (err) {
             console.error(err);
             process.exit(1);
         } else {
-            console.log('done:', image);
+            console.log('Done:', finalPath);
         }
     });
-});
+};
+
+const buildImages = (fromDir, toDir) => {
+    // STEP 1: scan source directory for directories structure and recreate it
+    // in destination directory to avoid nonexist-dir errors
+    const allDirs = getDirsFromDir(fromDir);
+    console.log('\nFound dirs:\n');
+    allDirs.forEach((directory) => {
+        console.log(directory);
+    });
+    allDirs.forEach((directory) => {
+        const finalPath = directory.replace(fromDir, toDir);
+        createDir(finalPath);
+    });
+
+    // STEP 2: scan source directory for all files
+    const allImages = getFilesFromDir(fromDir);
+    console.log('\nFound images:\n');
+    allImages.forEach((image) => {
+        console.log(image);
+    });
+
+    // STEP 3: create predefined image sizes for each image found
+    console.log('\nStarting working on images\n');
+    allImages.forEach((imagePath) => {
+        console.log(imagePath);
+        convertImage(fromDir, toDir, imagePath, imageSizes.small);
+        convertImage(fromDir, toDir, imagePath, imageSizes.small_2x);
+    });
+};
+
+buildImages(sourceDirectory, destinationDirectory);
 
 /*
 todo:
-- create global function with source and dest directories as arguments? or at least put those in consts
-- define separate function for each image size:
-    - thumbnail
-    - thumbnail @2x
-    - original (should just copy)
-- image size function should rename with prefix
-- write function that adds prefix for image path
 - filter files list by image extensions array
 */
